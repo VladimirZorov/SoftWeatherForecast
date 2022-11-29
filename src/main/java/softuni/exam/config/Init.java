8c3c94babc446027182059a133f154e782f73369
Vladimir Zorov <vl.zorov@gmail.com>
2022-11-29 18:41:35 +0200

    Add

---
+++ src/main/java/softuni/exam/config/Init.java
@@ -0,0 +1,14 @@
+package softuni.exam.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Init implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
